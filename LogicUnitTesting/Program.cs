e7cb2a1ad64198f5e1daa33bf674af0563a965b4
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-02-01 01:46:42 +0530

    Finding Square root

--- LogicUnitTesting/Program.cs
+++ LogicUnitTesting/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LogicUnitTesting;
 Console.WriteLine("Hello, Welcome to Nunit testing progamming");
-Console.WriteLine("1. Vending Machine \n2. Temperatures Conversion");
+Console.WriteLine("1. Vending Machine \n2. Temperatures Conversion \n3. Finding SquareRoot");
 int option = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Press  's'   to start programe and 'n' to stop progrme");
 char ch = Convert.ToChar(Console.ReadLine()); 
@@ -22,6 +22,14 @@ while(ch != 'n')
             temperatures.faherenhite = Convert.ToInt32(Console.ReadLine());
             temperatures.Celcius();
             temperatures.Faheren();
+            break;
+        case 3:
+            Sqroot sqroot = new Sqroot();
+            double num;
+            Console.WriteLine("Enter a number for finding square root :- ");
+            num = Convert.ToDouble(Console.ReadLine());
+            double num2 = 0.00001;
+            sqroot.squareRoot(num, num2);
             break;
     }
 }
---
+++ LogicUnitTesting/Sqroot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicUnitTesting
+{
+    public class Sqroot
+    {
+        public void squareRoot(double num, double num2)
+        {
+            double x = num;
+            double root;
+            int count = 0;
+
+            while (true)
+            {
+                count++;
+                root = 0.5 * (x + (num / x));
+
+                if (Math.Abs(root - x) < num2)
+                    break;
+                x = root;
+            }
+
+            Console.WriteLine("Root "+ root);
+        }
+    }
+}
