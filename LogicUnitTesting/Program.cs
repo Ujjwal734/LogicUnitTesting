c4f32f2db0a227455ded565d3f55ee56bd4b1fd0
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-02-01 01:32:58 +0530

    Conversion of Temperature

--- LogicUnitTesting/Program.cs
+++ LogicUnitTesting/Program.cs
@@ -1,18 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LogicUnitTesting;
 Console.WriteLine("Hello, Welcome to Nunit testing progamming");
-Console.WriteLine("1. Vending Machine");
+Console.WriteLine("1. Vending Machine \n2. Temperatures Conversion");
 int option = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Press  's'   to start programe and 'n' to stop progrme");
 char ch = Convert.ToChar(Console.ReadLine()); 
 while(ch != 'n')
 {
-    switch(option)
+    switch (option)
     {
         case 1:
             Vending vending = new Vending();
             Console.WriteLine("Enter a amount how much you want a change :-");
             vending.Machine();
+            break;
+        case 2:
+            Temperatures temperatures = new Temperatures();
+            Console.WriteLine("Enter a degree in degree celsicus :- ");
+            temperatures.degreeC = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a degree in degree Faherenhite :- ");
+            temperatures.faherenhite = Convert.ToInt32(Console.ReadLine());
+            temperatures.Celcius();
+            temperatures.Faheren();
             break;
     }
 }
---
+++ LogicUnitTesting/Temperatures.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicUnitTesting
+{
+    public class Temperatures
+    {
+        public int degreeC, faherenhite;
+        double result = 0.0;
+        public void Celcius()
+        {
+            result = (degreeC * 9 / 5) + 32;
+            Console.WriteLine("Celsicius to faherenhite is :- " + result);
+        }
+        public void Faheren()
+        {
+            result = ((faherenhite - 32) * 5 / 9);
+            Console.WriteLine("Faherenhite to celsicus is :- " + result);
+        }
+    }
+}
