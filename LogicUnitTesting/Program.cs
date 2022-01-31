a6191108557e55b007859224aaffc2872dcfde17
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-02-01 01:19:54 +0530

    Vending machine

--- LogicUnitTesting/Program.cs
+++ LogicUnitTesting/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LogicUnitTesting;
+Console.WriteLine("Hello, Welcome to Nunit testing progamming");
+Console.WriteLine("1. Vending Machine");
+int option = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Press  's'   to start programe and 'n' to stop progrme");
+char ch = Convert.ToChar(Console.ReadLine()); 
+while(ch != 'n')
+{
+    switch(option)
+    {
+        case 1:
+            Vending vending = new Vending();
+            Console.WriteLine("Enter a amount how much you want a change :-");
+            vending.Machine();
+            break;
+    }
+}
---
+++ LogicUnitTesting/Vending.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicUnitTesting
+{
+    public class Vending
+    {
+        public void Machine()
+        {
+            int amount = Convert.ToInt32(Console.ReadLine());
+            int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int[] noteCounter = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount - noteCounter[i] * notes[i];
+                }
+            }
+            Console.WriteLine("Currency Count ->");
+            for (int i = 0; i < 8; i++)
+            {
+                if (noteCounter[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : "
+                        + noteCounter[i]);
+                }
+            }
+        }
+    }
+}
